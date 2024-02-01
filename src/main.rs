0e08a770c87824c7eb2b1f00e475092f9670e5b3
acf-patrick <miharisoap@gmail.com>
2024-02-01 16:43:41 +0300

    added some test functions

--- src/main.rs
+++ src/main.rs
@@ -50,11 +50,6 @@ pub fn test_runner(tests: &[&dyn Testable]) {
     exit_qemu(QemuExitCode::Success);
 }
 
-#[test_case]
-fn trivial_assertion() {
-    assert_eq!(1, 1);
-}
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World");
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -146,16 +146,25 @@ impl fmt::Write for Writer {
     }
 }
 
-/* pub fn print_something() {
-    use core::fmt::Write;
-    let mut writer = Writer {
-        column_position: 0,
-        color_code: ColorCode::new(Color::Yellow, Color::Black),
-        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
-    };
-
-    writer.write_byte(b'H');
-    writer.write_string("ello! ");
-    write!(writer, "The numbers are {} and {}", 42, 1.0 / 3.0).unwrap();
+#[test_case]
+fn test_println() {
+    println!("Hello world!");
 }
- */
+
+#[test_case]
+fn test_println_many() {
+    for i in 0..200 {
+        println!("{i}");
+    }
+}
+
+#[test_case]
+fn test_println_output() {
+    let string = "Some test string that fits on a single line";
+    println!("{string}");
+
+    for (i, c) in string.chars().enumerate() {
+        let screen_char = WRITER.lock().buffer.chars[BUFFER_HEIGHT - 2][i].read();
+        assert_eq!(char::from(screen_char.ascii_character), c);
+    }
+}
